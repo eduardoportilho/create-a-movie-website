8083537448beeddab7296eaa9fe91c0e8b28be8f
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2018-04-26 07:00:04 +0200

    Store movie release year

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -133,7 +133,7 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h2>{movie_title} ({movie_year})</h2>
     <div>
       <span class="rating-star glyphicon glyphicon-star-empty" aria-hidden="true"></span>
       <span class="rating-star glyphicon glyphicon-star-empty" aria-hidden="true"></span>
@@ -156,6 +156,7 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
+            movie_year=movie.year,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
--- movie.py
+++ movie.py
@@ -3,15 +3,17 @@ import webbrowser
 class Movie():
     """Class that stores movie data."""
 
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, year, poster_image_url, trailer_youtube_url):
         """Initialize an instance of the class `Movie` with the movie data.
            
         Args:
           title (str): Movie title.
+          year (int): Year is which the movie was released.
           poster_image_url (str): URL for the image of the movie poster.
           trailer_youtube_url (str): URL for the movie trailer on Youtube.
         """
         self.title = title
+        self.year = year
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
   
